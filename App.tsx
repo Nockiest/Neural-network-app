32d6e09645fc6b1b575ee1aa7574859836213eef
Nockiest <115572770+Nockiest@users.noreply.github.com>
2023-03-24 19:19:59 +0100

    2 files modified

--- App.tsx
+++ App.tsx
@@ -4,8 +4,6 @@ import Toolbar from "./Toolbar.js"
 import Workspace from "./workspace.js"
 import { useState } from 'react';
 
-import { useState } from 'react';
-
 function App() {
   const [isToolbarVisible, setIsToolbarVisible] = useState(true);
 
@@ -15,19 +13,15 @@ function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row', height: '100vh' }}>
-      {isToolbarVisible && (
+      {
         <div style={{ width: '200px', backgroundColor: 'lightgray', position:"fixed", zIndex: 1 }}>
-          <Toolbar toggleVisible={toggleToolbar} />
+          <Toolbar toggleVisible={toggleToolbar}
+                   isVisible={isToolbarVisible} />
         </div>
-      )}
+      }
       <div style={{ flex: 1, position: 'relative', zIndex: 0 }}>
         <Workspace />
       </div>
-      {!isToolbarVisible && (
-        <div style={{ position: 'fixed', top: 0, right: 0 }}>
-          <button onClick={toggleToolbar}>Show Toolbar</button>
-        </div>
-      )}
     </div>
   );
 }
@@ -38,7 +32,7 @@ export default App;
 
 
 //nastav pozici sliderů rovnou v return statement neuronu
-// zobraz lištu nastavení nalevé straně, 
+ 
 //oprace problém s toggle tlačítkem
 // zamez zobrazování neuronu na druhém neuronu
 // vytvoř dva různé slidery, jeden na bias a druhý na váhu
--- Toolbar.js
+++ Toolbar.js
@@ -1,34 +1,52 @@
 import * as React from 'react';
-
 import { useState } from 'react';
 
 function Toolbar(props) {
-  const [isToolbarVisible, setIsToolbarVisible] = useState(true);
+  const { isVisible, toggleVisible } = props;
 
   const toggleToolbar = () => {
-    setIsToolbarVisible(!isToolbarVisible);
-    props.toggleVisible();
+    toggleVisible();
   };
 
   return (
-    <div id="toolbar" style={{ position: 'absolute', top: 0, right: 0, height: '100vh' }}>
-      <div style={{ position: 'absolute', top: 0, right: 0 }}>
-        {isToolbarVisible && (
-          <button onClick={toggleToolbar}>Hide Toolbar</button>
-        )}
-        {!isToolbarVisible && (
-          <button onClick={toggleToolbar}>Hide</button>
-        )}
+    <div
+      id="toolbar"
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: isVisible ? '20%' : '5%', // smaller when retracted
+        bottom:  0 , // fixed the typo here
+        backgroundColor: 'darkgrey',
+        padding: isVisible ? '20px' : '0 20px', // no padding when retracted
+        transition: 'width 0.3s, padding 0.3s', // animate the transition
+      }}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)'
+        }}
+      >
+        <button onClick={toggleToolbar}>
+          {isVisible ? 'Hide Toolbar' : 'Show Toolbar'}
+        </button>
       </div>
-      <div style={{ display: isToolbarVisible ? 'block' : 'none' }}>
-        <p>Save</p>
-        <p>Delete</p>
-        <p>New</p>
-        <p>Load a network</p>
-        <p>scale</p>
-      </div>
+      {isVisible && (
+        <div>
+          <p style={{ marginBottom: '20px' }}>Save</p>
+          <p style={{ marginBottom: '20px' }}>Delete</p>
+          <p style={{ marginBottom: '20px' }}>New</p>
+          <p style={{ marginBottom: '20px' }}>Load a network</p>
+          <p style={{ marginBottom: '20px' }}>scale</p>
+        </div>
+      )}
     </div>
   );
 }
+
+
 
 export default Toolbar;
